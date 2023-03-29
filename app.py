526b466d27e41e885c5a88e90c5cc5dc93b7fbbe
Aashay Kaurav <112794592+aashayKaurav@users.noreply.github.com>
2023-03-29 11:27:37 +0000

    Store application form in database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -21,6 +21,18 @@ def show_job(id):
     return "Not Found", 404
   return render_template('jobpage.html', job=job)
 
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  #collecting data through post method using 'form'
+  data = request.form
+  job = load_job_from_db(id)
+  #Storing data collected through form in database
+  add_application_to_db(id, data)
+  #render back data to html page inorder to acknowledge the applicant
+  return render_template('application_submitted.html', 
+                        application=data,
+                        job=job)
+  
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
